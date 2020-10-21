d68c5e7ef3cd4cfb575d499d35078c0b79758aa2
Magnus Dilling <magnusdilling@gmail.com>
2020-10-21 23:15:09 +0200

    Post new users to DB

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
 
 def signupuser(request):
-
-    return render(request, 'todo/signupuser.html', {'form':UserChangeForm})
+    if request.method =='GET':
+        return render(request, 'todo/signupuser.html', {'form':UserChangeForm})
+    else:
+        # Create a new user
+        user = User.objects.create_user(
+            request.POST['username'], 
+            request.POST['email'])
+        user.save
