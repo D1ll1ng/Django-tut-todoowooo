847e564aba76c84c5df398e3fd8f03fc5909880c
Magnus Dilling <magnusdilling@gmail.com>
2020-10-22 17:36:06 +0200

    new current page and logout text in header if logged in and add base template then is new being extended from in the other templates

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 def signupuser(request):
     if request.method =='GET':
         return render(request, 'todo/signupuser.html', {'form':UserChangeForm})
     else:
         # Create a new user
-        user = User.objects.create_user(
-            request.POST['username'], 
-            request.POST['email'])
-        user.save
+        try:
+            user = User.objects.create_user(
+                request.POST['username'], 
+                request.POST['email'])
+            user.save
+            login(request, user)
+            return redirect('currenttodos')
+
+        except IntegrityError:
+            return render(request, 'todo/signupuser.html', {'form':UserChangeForm(), 'error': 'U fucking dummy! The username is already taken, man up!'})
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
