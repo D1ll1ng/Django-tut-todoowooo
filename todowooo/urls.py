6276b8f6a9b315bcacc6de11ccdfe881beafe29c
Magnus Dilling <magnusdilling@gmail.com>
2020-11-11 17:50:27 +0100

    loads of changes

--- todowooo/urls.py
+++ todowooo/urls.py
@@ -1,5 +1,4 @@
-"""todowooo URL Configuration
-
+"""todowoo URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -29,4 +28,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.createtodo, name='createtodo'),
     path('current/', views.currenttodos, name='currenttodos'),
+    path('completed/', views.completedtodos, name='completedtodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 ]
